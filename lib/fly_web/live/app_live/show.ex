04d96c79b4f7cb13c719a4de542d3106d2dddfe8
Vince Roy <vincer0117@gmail.com>
2021-09-06 11:37:59 -0400

    integrate deployment and allocations into app view

--- lib/fly_web/live/app_live/show.ex
+++ lib/fly_web/live/app_live/show.ex
@@ -3,27 +3,9 @@ defmodule FlyWeb.AppLive.Show do
   require Logger
 
   alias Fly.Client
+  alias FlyWeb.Components.App
   alias FlyWeb.Components.HeaderBreadcrumbs
-
-  @impl true
-  def mount(%{"name" => name}, session, socket) do
-    socket =
-      assign(socket,
-        config: client_config(session),
-        state: :loading,
-        app: nil,
-        app_name: name,
-        count: 0,
-        authenticated: true
-      )
-
-    # Only make the API call if the websocket is setup. Not on initial render.
-    if connected?(socket) do
-      {:ok, fetch_app(socket)}
-    else
-      {:ok, socket}
-    end
-  end
+  alias FlyWeb.Components.Headings
 
   defp client_config(session) do
     Fly.Client.config(access_token: session["auth_token"] || System.get_env("FLYIO_ACCESS_TOKEN"))
@@ -46,9 +28,70 @@ defmodule FlyWeb.AppLive.Show do
     end
   end
 
+  @spec format_allocation_checks(any) :: binary
+  def format_allocation_checks(checks) when is_list(checks) do
+    checks
+    |> Enum.reduce([0, 0], fn c, [total, passing] ->
+      [
+        total + 1,
+        passing + (c["status"] === "passing" && 1 || 0)
+      ]
+    end)
+    |> then(fn [total, passing] ->
+      "#{total} total, #{passing} passing"
+    end)
+  end
+  def format_allocation_checks(_), do: ""
+
+  def format_process_group_regions([]), do: "-"
+  def format_process_group_regions(regions) when is_list(regions), do: Enum.join(regions, ", ")
+  def format_process_group_regions(_), do: "-"
+
+  def format_timestamp(ts) do
+    with {:ok, ts} <- Timex.parse(ts, "{ISO:Extended}"),
+      {:ok, ts} <- Timex.format(ts, "{relative}", :relative) do
+     ts
+    else
+      _ -> ts
+    end
+  end
+
+  @impl true
+  def mount(%{"name" => name}, session, socket) do
+    socket =
+      assign(socket,
+        config: client_config(session),
+        state: :loading,
+        app: nil,
+        app_name: name,
+        count: 0,
+        authenticated: true
+      )
+
+    # Only make the API call if the websocket is setup. Not on initial render.
+    if connected?(socket) do
+      {:ok, fetch_app(socket)}
+    else
+      {:ok, socket}
+    end
+  end
+
+
   @impl true
   def handle_event("click", _params, socket) do
     {:noreply, assign(socket, count: socket.assigns.count + 1)}
+  end
+
+  def handle_event("refresh", _params, socket) do
+    {:noreply, fetch_app(socket)}
+  end
+
+  def labels_allocations do
+    ["Task", "Version", "Region", "Desired", "Status", "Health Checks", "Restarts", "Created", "ID"]
+  end
+
+  def preview_url(app) do
+    "https://#{app["name"]}.fly.dev"
   end
 
   def status_bg_color(app) do
@@ -67,7 +110,20 @@ defmodule FlyWeb.AppLive.Show do
     end
   end
 
-  def preview_url(app) do
-    "https://#{app["name"]}.fly.dev"
+  def status_deployment_bg_color(deployment) do
+    case deployment["status"] do
+      "successful" -> "bg-green-100"
+      "failed" -> "bg-red-100"
+      _ -> "bg-yellow-100"
+    end
+  end
+
+  @spec status_deployment_text_color(nil | maybe_improper_list | map) :: <<_::64, _::_*8>>
+  def status_deployment_text_color(deployment) do
+    case deployment["status"] do
+      "successful" -> "text-green-800"
+      "failed" -> "text-red-800"
+      _ -> "text-yellow-800"
+    end
   end
 end
